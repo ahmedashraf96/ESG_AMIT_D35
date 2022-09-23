b01f3d1c5f01c301a9ea7ac53c5aa34cfe0c7afa
Ahmed Ashraf <ahmedashrafelnaqeeb@gmail.com>
2022-09-23 16:11:18 +0200

    Adding seven segment timin frames

--- Interfacing/main.c
+++ Interfacing/main.c
@@ -5,18 +5,43 @@
 
 int main(void)
 {
+    u8_t au8_looping;
+    u8_t au8_looping_2;
+
+    u8_t big_number = 0;
+
     hsev_seg_init(SEV_SEG_1);
-    hsev_seg_enable(SEV_SEG_1);
-    //hsev_seg_disable(SEV_SEG_2);
+    hsev_seg_init(SEV_SEG_2);
 
     while(1)
     {
-        u8_t au8_looping;
         for(au8_looping = NUMBER_0; au8_looping <= NUMBER_9; au8_looping++)
         {
-            hsev_seg_displayNumber(au8_looping);
-            _delay_ms(1000);
+            for(au8_looping_2 = 0; au8_looping_2 < 50; au8_looping_2++)
+            {
+                hsev_seg_enable(SEV_SEG_1);
+                hsev_seg_displayNumber(big_number);
+                _delay_ms(10);
+                hsev_seg_disable(SEV_SEG_1);
+
+                hsev_seg_enable(SEV_SEG_2);
+                hsev_seg_displayNumber(au8_looping);
+                _delay_ms(10);
+                hsev_seg_disable(SEV_SEG_2);
+            }
         }
+
+        big_number++;
+
+        if(big_number > NUMBER_9)
+        {
+            big_number = NUMBER_0;
+        }
+        else
+        {
+            /*Do nothing*/
+        }
+
     }
 
     return 0;
