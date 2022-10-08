129f38b3f49eac151211248f6600a6792076f668
Ahmed Ashraf <ahmedashrafelnaqeeb@gmail.com>
2022-10-08 15:47:19 +0200

    Adding timer application

--- Interfacing/main.c
+++ Interfacing/main.c
@@ -1,41 +1,27 @@
 #include "LSTD_TYPES.h"
 #include "LBIT_MATH.h"
-#include "MDIO_interface.h"
-#include "MADC_interface.h"
-
+#include "avr/io.h"
 
 int main(void)
 {
-    u16_t au16_value;
-
-    mdio_setPinStatus(PORTC, (PIN3 | PIN7), OUTPUT);
-    mdio_setPinStatus(PORTD, PIN3, OUTPUT);    
-    mdio_setPinStatus(PORTA, PIN1, INPUT_FLOAT);
-
-    madc_init(AVCC_MODE, ADC_10_BITS, ADC_PRESCALER_16);
+    u16_t au16_value = 0;
+    SET_BIT(DDRC, 7);
+    TCCR0 = 0;
+    TCNT0 = 113;
+    TCCR0 |= 3;
 
     while(1)
     {
-        madc_singleConversion(ADC_CHANNEL_1, &au16_value);
+        TOGGLE_BIT(PORTC, 7);
 
-        if(au16_value >= 0 && au16_value <= 250)
+        while(au16_value < 976)
         {
-            mdio_setPinValue(PORTC, PIN3, HIGH);
-            mdio_setPinValue(PORTC, PIN7, LOW);
-            mdio_setPinValue(PORTD, PIN3, LOW);
+            while(!GET_BIT(TIFR, 0));
+            SET_BIT(TIFR, 0);
+            au16_value++;
         }
-        else if(au16_value > 250 && au16_value <= 700)
-        {
-            mdio_setPinValue(PORTC, PIN3, LOW);
-            mdio_setPinValue(PORTC, PIN7, HIGH);
-            mdio_setPinValue(PORTD, PIN3, LOW);
-        }
-        else
-        {
-            mdio_setPinValue(PORTC, PIN3, LOW);
-            mdio_setPinValue(PORTC, PIN7, LOW);
-            mdio_setPinValue(PORTD, PIN3, HIGH);            
-        }
+        au16_value = 0;
+        
     }
 
     return 0;
