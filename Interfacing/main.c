c754adaafee582ad337dc0e373c6fc8811be1db4
Ahmed Ashraf <ahmedashrafelnaqeeb@gmail.com>
2022-10-14 17:49:07 +0200

    Adding PWM motor application

--- Interfacing/main.c
+++ Interfacing/main.c
@@ -2,15 +2,35 @@
 #include "LBIT_MATH.h"
 #include "MDIO_interface.h"
 #include "MTIMER_interface.h"
+#include "util/delay.h"
 
 int main(void)
 {
+    u16_t delay_var = 0;
+
     mdio_setPinStatus(PORTB, PIN3, OUTPUT);
-    mtimer_init(TIMER_CHANNEL_0, TIMER_PWM_MODE, TIMER_FREQ_500_HZ);
+    mtimer_init(TIMER_CHANNEL_0, TIMER_PWM_MODE, TIMER_FREQ_1KHZ);
 
     mtimer_startPWM_signal(TIMER_CHANNEL_0);
-    mtimer_runPWM_signal(TIMER_CHANNEL_0, 50);
-    while(1);
+    while(1)
+    {
+        for(delay_var = 0; delay_var <= 255; delay_var++)
+        {
+            mtimer_runPWM_signal(TIMER_CHANNEL_0, delay_var);
+            _delay_ms(50);
+        }
+
+        _delay_ms(3000);
+
+        for(delay_var = 255; delay_var >= 0; delay_var--)
+        {
+            mtimer_runPWM_signal(TIMER_CHANNEL_0, delay_var);
+            _delay_ms(50);
+        }
+
+        _delay_ms(3000);
+    }
+
 
     return 0;
 }
