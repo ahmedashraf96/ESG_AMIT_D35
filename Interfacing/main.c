3228bfeb084ece9d1cd10c460a6e19f1929a7f66
Ahmed Ashraf <ahmedashrafelnaqeeb@gmail.com>
2022-10-15 17:51:56 +0200

    Adding WDT application

--- Interfacing/main.c
+++ Interfacing/main.c
@@ -1,29 +1,21 @@
 #include "LSTD_TYPES.h"
 #include "LBIT_MATH.h"
-#include "avr/io.h"
+#include "LWDT.h"
 #include "MDIO_interface.h"
-#include "MTIMER_interface.h"
+#include "util/delay.h"
 
 int main(void)
 {
-    u8_t duty;
-    u32_t freq;
+    mdio_setPinStatus(PORTA, PIN0, OUTPUT);
 
-    mdio_setPinStatus(PORTB, PIN3, OUTPUT);
-    mdio_setPinStatus(PORTD, PIN6, INPUT_FLOAT);
-    
-    mtimer_init(TIMER_CHANNEL_0, TIMER_PWM_MODE, TIMER_FREQ_1KHZ);
-    mtimer_init(TIMER_CHANNEL_1, TIMER_ICU_MODE, TIMER_ICU_PRESCALER);
-
-    mtimer_startPWM_signal(TIMER_CHANNEL_0);
-    mtimer_runPWM_signal(TIMER_CHANNEL_0, 230);
+    WDT_START(TIMEOUT_2_1_S);
+    _delay_ms(1500);        
+    WDT_STOP();
     
     while(1)
     {
-        mtimer_getSignal_duty_and_freq(&duty, &freq);
-
-        TCNT2 = duty;
-        OCR1A = (u16_t)freq;
+        mdio_togglePinValue(PORTA, PIN0);
+        _delay_ms(1000);        
     }
 
     return 0;
