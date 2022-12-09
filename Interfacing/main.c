89aa3fed85ad7d8c6ab885c8e8d4deddd7bf0528
VNET\aelnaqee <ahmed.elnaqeeb@valeo.com>
2022-12-09 17:28:47 +0200

    Adding event group bits example

--- Interfacing/main.c
+++ Interfacing/main.c
@@ -4,16 +4,28 @@
 #include "HPBUTT_interface.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "queue.h"
+#include "event_groups.h"
 
-QueueHandle_t xQueueHandler;
+#define LED0_EVENT    (1 << 0)
+#define LED1_EVENT    (1 << 1)
+#define LED2_EVENT    (1 << 2)
+
+EventGroupHandle_t xEventGroup_handler;
+EventBits_t xEventGroupBits;
 
 void taskInit(void* pvParameters)
 {
     while(1)
     {
-        xQueueHandler = xQueueCreate(1, sizeof(u8_t));
+        xEventGroup_handler = xEventGroupCreate();
+
         hled_init(LED0);
+        hled_init(LED1);
+        hled_init(LED2);
+
+        hpbutt_init(PUSH_BUTTON_0);
+        hpbutt_init(PUSH_BUTTON_1);
+        hpbutt_init(PUSH_BUTTON_2);
 
         vTaskDelete(NULL);
     }
@@ -21,60 +33,88 @@ void taskInit(void* pvParameters)
     return;
 }
 
-void msgTask1(void* pvParameters)
+void pbTask(void* pvParameters)
 {
-    static u8_t x = 'A';
+    u8_t pb0_status = 0;
+    u8_t pb1_status = 0;
+    u8_t pb2_status = 0;
+    u8_t eventsToSet = 0;
 
     while(1)
     {
-        if(x == 'A')
+        hpbutt_getStatus(PUSH_BUTTON_0, &pb0_status);
+        hpbutt_getStatus(PUSH_BUTTON_1, &pb1_status);
+        hpbutt_getStatus(PUSH_BUTTON_2, &pb2_status);
+
+        if(pb0_status == PRESSED)
         {
-            x = 'B';
-        }
-        else if(x == 'B')
-        {
-            x = 'A';
+            SET_BIT(eventsToSet, 0);
         }
         else
         {
             /*Do nothing*/
         }
 
-        xQueueSend(xQueueHandler, (const void*)&x, (TickType_t)10);
+        if(pb1_status == PRESSED)
+        {
+            SET_BIT(eventsToSet, 1);
+        }
+        else
+        {
+            /*Do nothing*/
+        }
 
-        vTaskDelay(1000);
+        if(pb2_status == PRESSED)
+        {
+            SET_BIT(eventsToSet, 2);
+        }
+        else
+        {
+            /*Do nothing*/
+        }
+
+        xEventGroupSetBits(xEventGroup_handler, eventsToSet);
+
+        vTaskDelay(100);
     }
 
     return;
 }
 
-void msgTask2(void* pvParameters)
-{
-    u8_t tempData;
-  
+void ledTask(void* pvParameters)
+{  
     while(1)
     {
-        if( xQueueReceive(xQueueHandler, (void* const)&tempData, (TickType_t)10) == pdTRUE )
+        // Task will be blocked until one of the pb is pressed
+        xEventGroupWaitBits(xEventGroup_handler, (LED0_EVENT | LED1_EVENT | LED2_EVENT), pdTRUE, pdFALSE, portMAX_DELAY);
+        xEventGroupBits = xEventGroupGetBits(xEventGroup_handler);
+
+        if(GET_BIT(xEventGroupBits, 0) == pdTRUE)
         {
-            if(tempData == 'A')
-            {
-                hled_ledValueON(LED0);
-            }
-            else if(tempData == 'B')
-            {
-                hled_ledValueOFF(LED0);
-            }
-            else
-            {
-                /*Do nothing*/
-            }
+            hled_toggleLedValue(LED0);
         }
         else
         {
             /*Do nothing*/
         }
 
-        vTaskDelay(1000);
+        if(GET_BIT(xEventGroupBits, 1) == pdTRUE)
+        {
+            hled_toggleLedValue(LED1);
+        }
+        else
+        {
+            /*Do nothing*/
+        }
+
+        if(GET_BIT(xEventGroupBits, 2) == pdTRUE)
+        {
+            hled_toggleLedValue(LED2);
+        }
+        else
+        {
+            /*Do nothing*/
+        }
     }
 
     return;
@@ -84,8 +124,8 @@ void msgTask2(void* pvParameters)
 int main(void)
 {
     xTaskCreate(taskInit, "Init", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
-    xTaskCreate(msgTask1, "T1", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-    xTaskCreate(msgTask2, "T2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(pbTask, "T1", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    xTaskCreate(ledTask, "T2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
